7d5aa184e869e34fe591176ea324ac74fb474111
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2025-09-01 12:13:32 -0500

    feat: :construction: Cambio de estructura inicial

--- src/services/userService.js
+++
@@ -1,8 +0,0 @@
-import client from "../config/db.js";
-
-const db = client.db(process.env.DB_NAME);
-const users = db.collection("usuarios");
-
-export const findUserByEmail = async (email) => {
-  return users.findOne({ email });
-};
